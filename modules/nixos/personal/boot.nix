20a4fd111fc0befab30074ba054b04438b774a32
Quentin Aristote <quentin@aristote.fr>
2023-02-17 21:48:17 +0100

    split config between grub and efi

--- modules/nixos/personal/boot.nix
+++ modules/nixos/personal/boot.nix
@@ -2,16 +2,23 @@
 
 let cfg = config.personal.boot;
 in {
-  options.personal.boot = { grub.enable = lib.mkEnableOption "grub"; };
-
-  config.boot.loader = lib.mkIf cfg.grub.enable {
-    efi = { canTouchEfiVariables = true; };
-    grub = {
-      enable = true;
-      version = 2;
-      efiSupport = true;
-      enableCryptodisk = config.boot.initrd.luks.devices != { };
-      device = "nodev";
-    };
+  options.personal.boot = {
+    grub.enable = lib.mkEnableOption "grub";
+    efi.enable = lib.mkEnableOption "EFI";
   };
+
+  config.boot.loader = lib.mkMerge [
+    (lib.mkIf cfg.grub.enable {
+      grub = {
+        enable = true;
+        version = 2;
+        enableCryptodisk = config.boot.initrd.luks.devices != { };
+        device = lib.mkDefault "nodev";
+      };
+    })
+    (lib.mkIf cfg.efi.enable {
+      efi.canTouchEfiVariables = true;
+      grub.efiSupport = true;
+    })
+  ];
 }
