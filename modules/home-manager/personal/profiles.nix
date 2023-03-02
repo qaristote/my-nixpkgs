e8d6bbd026adb8e8aee6ca28d2fc6f184517e830
Quentin Aristote <quentin@aristote.fr>
2023-03-02 16:40:58 +0100

    home: email: properly set folders in thunderbird

--- modules/home-manager/personal/profiles.nix
+++ modules/home-manager/personal/profiles.nix
@@ -104,10 +104,10 @@ in {
           userName = "quentin@aristote.fr";
           realName = "Quentin Aristote";
           folders = {
-            drafts = "Inbox/Brouillons";
-            inbox = "Inbox";
-            sent = "Inbox/Envoyés";
-            trash = "Inbox/Corbeille";
+            drafts = "INBOX/Brouillons";
+            inbox = "INBOX";
+            sent = "INBOX/Envoyés";
+            trash = "INBOX/Corbeille";
           };
           imap = {
             host = "ssl0.ovh.net";
@@ -120,7 +120,17 @@ in {
           thunderbird = {
             enable = true;
             profiles = [ "default" ];
-            settings = thunderbirdSettings;
+            settings = id:
+              thunderbirdSettings id // {
+                "mail.identity.id_${id}.draft_folder" =
+                  "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Brouillons";
+                "mail.identity.id_${id}.fcc_folder" =
+                  "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Envoy&AOk-s";
+                "mail.identity.id_${id}.archive_folder" =
+                  "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Archive";
+                "mail.server.server_${id}.trash_folder_name" =
+                  "INBOX/Corbeille";
+              };
           };
         };
         work = lib.mkIf cfg.social.identities.work {
@@ -150,7 +160,12 @@ in {
           thunderbird = {
             enable = true;
             profiles = [ "default" ];
-            settings = thunderbirdSettings;
+            settings = id: thunderbirdSettings id // {
+                "mail.identity.id_${id}.archive_folder" =
+                  "imap://qaristote@clipper.ens.fr/Archive";
+                "mail.server.server_${id}.trash_folder_name" =
+                  "Trash";
+            };
           };
         };
       };
