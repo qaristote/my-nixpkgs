34ece152e5532f83ce1bd43851f282ec44fcf6c9
quentin@aristote.fr <quentin@aristote.fr>
2023-03-07 16:20:07 +0100

    pkgs: userjs: thunderbird: update to v102.2

--- pkgs/static/userjs/thunderbird/default.nix
+++ pkgs/static/userjs/thunderbird/default.nix
@@ -1,8 +1,8 @@
 { fetchurl }:
 
-let version = "102.1";
+let version = "102.2";
 in fetchurl {
   url =
     "https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/v${version}/user.js";
-  sha256 = "1zid28fjz86a82pg21xn7icxgbv7qqwrhy7nam4cp6d9jj16nr4x";
+  sha256 = "b4cUbZ8aN4T0KwoHSijsRADt0egZZSnXNb1lY3KodKc=";
 }
