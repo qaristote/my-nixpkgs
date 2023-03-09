e19b95821d16f44a132e5eb9a6a52bd3b3efd978
quentin@aristote.fr <quentin@aristote.fr>
2023-03-09 10:56:00 +0100

    home: rofi: fix setting of font

--- modules/home-manager/personal/programs/rofi.nix
+++ modules/home-manager/personal/programs/rofi.nix
@@ -4,5 +4,7 @@
   programs.rofi = {
     cycle = lib.mkDefault true;
     theme = lib.mkDefault config.personal.home.dotfiles.rofi;
+    # TODO: remove this when stylix fixes it upstream
+    font = lib.mkIf (config ? stylix) (lib.mkForce "${config.stylix.fonts.monospace.name} 12"); 
   };
 }
