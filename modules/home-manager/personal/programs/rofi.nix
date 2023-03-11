8ff582da73b9c4b401dfdfedecba5d7b0172f101
quentin@aristote.fr <quentin@aristote.fr>
2023-03-11 14:58:06 +0100

    home: rofi: add plugins

--- modules/home-manager/personal/programs/rofi.nix
+++ modules/home-manager/personal/programs/rofi.nix
@@ -1,10 +1,11 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   programs.rofi = {
     cycle = lib.mkDefault true;
     theme = lib.mkDefault config.personal.home.dotfiles.rofi;
     # TODO: remove this when stylix fixes it upstream
-    font = lib.mkIf (config ? stylix) (lib.mkForce "${config.stylix.fonts.monospace.name} 12"); 
+    font = lib.mkIf (config ? stylix) (lib.mkForce "${config.stylix.fonts.monospace.name} 12");
+    plugins = with pkgs; [ rofi-top rofi-calc rofi-emoji ];
   };
 }
