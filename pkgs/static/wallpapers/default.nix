4f20f2983a97a427fd1bd7ca649cde37a3e935e2
qaristote <quentin.aristote@ens.fr>
2023-03-14 13:17:10 +0100

    pkgs: static: wallpapers: make overridable

--- pkgs/static/wallpapers/default.nix
+++ pkgs/static/wallpapers/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, imagemagick, lib }:
 
 let
-  fetchWallpaper =
+  fetchWallpaper = lib.makeOverridable (
     { name, url, sha256, resolution ? "1920x1080", offset ? "0x0" }:
     stdenv.mkDerivation {
       inherit name;
@@ -15,7 +15,7 @@ let
                        -crop "${resolution}+${offset}" \
                 "$out"
       '';
-    };
+    });
   sources = lib.importJSON ./sources.json;
 in {
   fetcher = fetchWallpaper;
