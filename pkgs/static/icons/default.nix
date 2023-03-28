ab89e2efcbed6bc0055ee344bf921427ff5c623d
qaristote <quentin.aristote@ens.fr>
2023-03-28 10:37:40 +0200

    pkgs: static: icons: update

--- pkgs/static/icons/default.nix
+++ pkgs/static/icons/default.nix
@@ -5,11 +5,11 @@
 		sha256 = "0b4gqhw9y62fm72x61q03yzbllgfxpkjbbsdvj7d5wg3jshjkgdb";
 	};
 	mubi = fetchurl {
-		url = "https://mubi.com/logo";
-		sha256 = "1h6qi579dcmd7l9mmwq2c4y67lbpkfjwq19kivfnfxwr38f769h4";
+		url = "https://mubi.com/MUBI-logo.png";
+		sha256 = "0fc53c8j6dvphykabqiy146hjmpnczm5rvlf92fycyiqgrg260c4";
 	};
 	deezer = fetchurl {
 		url = "https://raw.githubusercontent.com/edent/SuperTinyIcons/master/images/svg/deezer.svg";
-		sha256 = "1qcj1gqz8gc9cwlj4cl6yj5ik1vz4ya6qcncr5fbciprzaaf3pg9";
+		sha256 = "1h9m9rsg3hdr7w5g8g560rlwyy2wmk5a397cg1vr7gg37v9izh92";
 	};
 }
