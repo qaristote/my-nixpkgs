8887c7c81eedacd6caa88a45a87349e857cc3652
quentin@aristote.fr <quentin@aristote.fr>
2023-07-27 17:42:05 +0200

    firefox: update to v115

--- pkgs/static/userjs/arkenfox/default.nix
+++ pkgs/static/userjs/arkenfox/default.nix
@@ -1,8 +1,7 @@
 { fetchurl }:
 
-let version = "112.0";
+let version = "115.0";
 in fetchurl {
-  url =
-    "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
-  sha256 = "sha256-ZJ3HyM00hG3aVpOnrcjc/D75WsRj6rpKVRmmC3vp/4k=";
+  url = "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
+  sha256 = "sha256-sysEtq4aEWmkKy3KPe+4J/HJxjCxNcTAzptZ7s5JrJg=";
 }
