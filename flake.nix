323ca1dcf45938c9235a4f163d0d2bbcd3dab523
quentin@aristote.fr <quentin@aristote.fr>
2023-08-24 18:25:53 +0200

    add devenv

--- flake.nix
+++ flake.nix
@@ -4,37 +4,54 @@
     inputs.nixpkgs.url = "nixpkgs";
   };
 
-  outputs = { self, nur, nixpkgs, flake-parts, devenv, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [ flake-parts.flakeModules.easyOverlay devenv.flakeModule ];
-    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+  outputs = {
+    self,
+    nur,
+    nixpkgs,
+    flake-parts,
+    devenv,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [flake-parts.flakeModules.easyOverlay devenv.flakeModule];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
-    flake = {
-      devenvModules.personal = import ./modules/devenv;
-      nixosModules.personal = import ./modules/nixos;
-      homeModules.personal = import ./modules/home-manager;
-      # overlays.personal = self.overlays.default;
-    };
-
-    perSystem = { config, system, pkgs, lib, ... }: {
-      _module.args.pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ nur.overlay ];
-        config = {};
+      flake = {
+        devenvModules.personal = import ./modules/devenv;
+        nixosModules.personal = import ./modules/nixos;
+        homeModules.personal = import ./modules/home-manager;
+        overlays.personal = self.overlays.default;
       };
 
+      perSystem = {
+        config,
+        system,
+        pkgs,
+        lib,
+        ...
+      }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [nur.overlay];
+          config = {};
+        };
+
         overlayAttrs = {
-          inherit (lib.recursiveUpdate pkgs { personal = config.packages; lib.personal = config.packages.lib; }) personal lib;
+          inherit
+            (lib.recursiveUpdate pkgs {
+              personal = config.packages;
+              lib.personal = config.packages.lib;
+            })
+            personal
+            lib
+            ;
         };
         packages = import ./pkgs pkgs;
 
         devenv.shells.default = {
-          name = "my-nixpkgs";
-
-          imports = [ self.devenvModules.personal ];
-
+          imports = [self.devenvModules.personal];
           languages.nix.enable = true;
         };
+      };
     };
-  };
->>>>>>> Stashed changes
 }
