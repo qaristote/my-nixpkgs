2513104f959f9f4e0e80ac3decdfa06a67022795
aristote <quentin.aristote@irif.fr>
2023-09-01 15:04:03 +0200

    pkgs: userjs: arkenfox: fix sha

--- pkgs/static/userjs/arkenfox/default.nix
+++ pkgs/static/userjs/arkenfox/default.nix
@@ -3,5 +3,5 @@
 in
   fetchurl {
     url = "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
-    sha256 = "sha256-sysEtq4aEWmkKy3KPe+4J/HJxjCxNcTAzptZ7s5JrJg=";
+    sha256 = "sha256-pyJviSywIuDtM+yKVYLNn+TXCKmVI7au83SUVeGaHXQ=";
   }
