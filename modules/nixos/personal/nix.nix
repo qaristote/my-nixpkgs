5cacac7d3612b90523b66fe6ab4e99513e184467
quentin@aristote.fr <quentin@aristote.fr>
2023-12-03 20:35:26 +0100

    nixos: add trusted substituters

--- modules/nixos/personal/nix.nix
+++ modules/nixos/personal/nix.nix
@@ -22,6 +22,8 @@ in {
       settings = {
         auto-optimise-store = true;
         experimental-features = ["nix-command" "flakes"];
+        trusted-substituters = ["https://devenv.cachix.org/" "https://nix-community.cachix.org/"];
+        trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
       };
       gc = lib.mkIf cfg.gc.enable {
         automatic = true;
