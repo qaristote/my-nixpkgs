b281eb28a9d9c930234eadc25193a6f3f66cb83a
aristote <quentin.aristote@irif.fr>
2024-01-17 17:07:41 +0100

    pkgs: firefox addons: update and add floccus

--- modules/home-manager/personal/identities.nix
+++ modules/home-manager/personal/identities.nix
@@ -1,6 +1,9 @@
-{ config, lib, pkgs, ... }:
-
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   cfg = config.personal.identities;
   mkEnableIdentityOption = name: lib.mkEnableOption "${name} identity";
 in {
@@ -9,87 +12,96 @@ in {
     work = mkEnableIdentityOption "work";
   };
 
-  config = {
-    accounts.email.accounts = let
-      gpg = {
-        key = "DFC1660846EEA97C059F18534EF515441E635D36";
-        signByDefault = true;
+  config = lib.mkMerge [
+    {
+      accounts.email.accounts = let
+        gpg = {
+          key = "DFC1660846EEA97C059F18534EF515441E635D36";
+          signByDefault = true;
+        };
+        thunderbirdSettings = id: {
+          "mail.identity.id_${id}.fcc_folder_picker_mode" = 0;
+        };
+      in {
+        personal = lib.mkIf cfg.personal {
+          inherit gpg;
+          address = "quentin@aristote.fr";
+          userName = "quentin@aristote.fr";
+          realName = "Quentin Aristote";
+          folders = {
+            drafts = "INBOX/Brouillons";
+            inbox = "INBOX";
+            sent = "INBOX/Envoyés";
+            trash = "INBOX/Corbeille";
+          };
+          imap = {
+            host = "ssl0.ovh.net";
+            port = 993;
+          };
+          smtp = {
+            host = "ssl0.ovh.net";
+            port = 465;
+          };
+          thunderbird = {
+            enable = true;
+            profiles = ["default"];
+            settings = id:
+              thunderbirdSettings id
+              // {
+                "mail.identity.id_${id}.draft_folder" = "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Brouillons";
+                "mail.identity.id_${id}.fcc_folder" = "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Envoy&AOk-s";
+                "mail.identity.id_${id}.archive_folder" = "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Archive";
+                "mail.server.server_${id}.trash_folder_name" = "INBOX/Corbeille";
+              };
+          };
+        };
+        work = lib.mkIf cfg.work {
+          inherit gpg;
+          address = "quentin.aristote@irif.fr";
+          userName = "aristote";
+          realName = "Quentin Aristote";
+          aliases = ["aristote@irif.fr"];
+          folders = {
+            drafts = "Drafts";
+            inbox = "Inbox";
+            sent = "Sent";
+            trash = "Trash";
+          };
+          imap = {
+            host = "imap.irif.fr";
+            port = 993;
+          };
+          smtp = {
+            host = "smtp.irif.fr";
+            port = 465;
+          };
+          thunderbird = {
+            enable = true;
+            profiles = ["default"];
+            settings = id:
+              thunderbirdSettings id
+              // {
+                "mail.identity.id_${id}.archive_folder" = "imap://aristote@imap.irif.fr/Archive";
+                "mail.server.server_${id}.trash_folder_name" = "Trash";
+              };
+          };
+        };
       };
-      thunderbirdSettings = id: {
-        "mail.identity.id_${id}.fcc_folder_picker_mode" = 0;
+    }
+    (lib.mkIf cfg.work {
+      home = {
+        packages = with pkgs; [zotero evince];
+        file.".latexmkrc".source =
+          lib.mkDefault config.personal.home.dotfiles.latexmkrc;
       };
-    in {
-      personal = lib.mkIf cfg.personal {
-        inherit gpg;
-        address = "quentin@aristote.fr";
-        userName = "quentin@aristote.fr";
-        realName = "Quentin Aristote";
-        folders = {
-          drafts = "INBOX/Brouillons";
-          inbox = "INBOX";
-          sent = "INBOX/Envoyés";
-          trash = "INBOX/Corbeille";
+      programs.firefox.profiles = let
+        addFloccus = {
+          extensions = [pkgs.personal.firefoxAddons.floccus];
         };
-        imap = {
-          host = "ssl0.ovh.net";
-          port = 993;
-        };
-        smtp = {
-          host = "ssl0.ovh.net";
-          port = 465;
-        };
-        thunderbird = {
-          enable = true;
-          profiles = [ "default" ];
-          settings = id:
-            thunderbirdSettings id // {
-              "mail.identity.id_${id}.draft_folder" =
-                "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Brouillons";
-              "mail.identity.id_${id}.fcc_folder" =
-                "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Envoy&AOk-s";
-              "mail.identity.id_${id}.archive_folder" =
-                "imap://quentin%40aristote.fr@ssl0.ovh.net/INBOX/Archive";
-              "mail.server.server_${id}.trash_folder_name" = "INBOX/Corbeille";
-            };
-        };
+      in {
+        default = addFloccus;
+        videoconferencing = addFloccus;
       };
-      work = lib.mkIf cfg.work {
-        inherit gpg;
-        address = "quentin.aristote@irif.fr";
-        userName = "aristote";
-        realName = "Quentin Aristote";
-        aliases = [ "aristote@irif.fr" ];
-        folders = {
-          drafts = "Drafts";
-          inbox = "Inbox";
-          sent = "Sent";
-          trash = "Trash";
-        };
-        imap = {
-          host = "imap.irif.fr";
-          port = 993;
-        };
-        smtp = {
-          host = "smtp.irif.fr";
-          port = 465;
-        };
-        thunderbird = {
-          enable = true;
-          profiles = [ "default" ];
-          settings = id:
-            thunderbirdSettings id // {
-              "mail.identity.id_${id}.archive_folder" =
-                "imap://aristote@imap.irif.fr/Archive";
-              "mail.server.server_${id}.trash_folder_name" = "Trash";
-            };
-        };
-      };
-    };
-
-    home = lib.mkIf cfg.work {
-      packages = with pkgs; [ zotero evince ];
-      file.".latexmkrc".source =
-        lib.mkDefault config.personal.home.dotfiles.latexmkrc;
-    };
-  };
+    })
+  ];
 }
