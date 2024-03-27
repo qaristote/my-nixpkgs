ea1e4b48d625e9e504979964e7a3742008399b07
aristote <quentin.aristote@irif.fr>
2024-03-27 19:41:37 +0100

    nixos: nix: trusted-substituters -> substituters

--- modules/nixos/personal/nix.nix
+++ modules/nixos/personal/nix.nix
@@ -26,7 +26,7 @@ in {
       settings = {
         auto-optimise-store = true;
         experimental-features = ["nix-command" "flakes"];
-        trusted-substituters = ["https://devenv.cachix.org/" "https://nix-community.cachix.org/"];
+        substituters = ["https://devenv.cachix.org/" "https://nix-community.cachix.org/"];
         trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
       };
       gc = lib.mkIf cfg.gc.enable {
