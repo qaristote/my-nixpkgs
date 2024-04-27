qaristote/my-nixpkgs

commit 3dac1b87810540c3dd913a4f60962b1ef3bc5fa2

nixos: services.xserver.displayManager.defaultSession -> services.displayManager.defaultSession

modules/nixos/personal/gui.nix
@@ -49,12 +49,14 @@ in {
       };
     }
     (lib.mkIf cfg.i3.enable {
-      services.xserver = {
-        desktopManager.xfce = {
-          noDesktop = true;
-          enableXfwm = false;
+      services = {
+        xserver = {
+          desktopManager.xfce = {
+            noDesktop = true;
+            enableXfwm = false;
+          };
+          windowManager.i3.enable = true;
         };
-        windowManager.i3.enable = true;
         displayManager.defaultSession = "xfce+i3";
       };
     })

commit 84e3b5b66bac0549919336567515c9e87623c38f

nixos: gui: defaultSession: accomodate for <= 23.11
fix bug introduced in 3dac1b87810540c3dd913a4f60962b1ef3bc5fa2

modules/nixos/personal/gui.nix
@@ -48,18 +48,26 @@ in {
         autoRepeatDelay = 200;
       };
     }
-    (lib.mkIf cfg.i3.enable {
-      services = {
-        xserver = {
-          desktopManager.xfce = {
-            noDesktop = true;
-            enableXfwm = false;
+    (lib.mkIf cfg.i3.enable (
+      lib.mkMerge [
+        {
+          services = {
+            xserver = {
+              desktopManager.xfce = {
+                noDesktop = true;
+                enableXfwm = false;
+              };
+              windowManager.i3.enable = true;
+            };
           };
-          windowManager.i3.enable = true;
-        };
-        displayManager.defaultSession = "xfce+i3";
-      };
-    })
+        }
+        (
+          if (builtins.compareVersions lib.trivial.version "23.11" > 0)
+          then {services.defaultSession = "xfce+i3";}
+          else {services.xserver.displayManager.defaultSession = "xfce+i3";}
+        )
+      ]
+    ))
     (lib.mkIf cfg.stylix.enable ({
         assertions = let
           missingArgAssertion = name: {