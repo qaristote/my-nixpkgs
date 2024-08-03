028c26db490fea082df3d34becbac24fb822c612
aristote <quentin.aristote@irif.fr>
2024-08-03 21:32:36 +0200

    home: add network manager applet

--- modules/home-manager/personal/gui/x/i3/startup.nix
+++ modules/home-manager/personal/gui/x/i3/startup.nix
@@ -1,18 +1,26 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  ...
+} @ extraArgs: {
   xsession.windowManager.i3.config.startup = let
-    autostart = { command, always ? false, notification ? false }: {
+    autostart = {
+      command,
+      always ? false,
+      notification ? false,
+    }: {
       inherit command always notification;
     };
     autostartIf = cond: args: lib.optional cond (autostart args);
-  in [
-    (autostart { command = "rfkill block bluetooth"; })
-    (autostart { command = "keepassxc"; })
-  ]
-  ++ autostartIf config.programs.thunderbird.enable { command = "thunderbird"; }
-  ++ autostartIf
-  (config.personal.profiles.social && config.personal.identities.personal) {
-    command = "signal-desktop";
-  };
+  in
+    [
+      (autostart {command = "rfkill block bluetooth";})
+      (autostart {command = "keepassxc";})
+    ]
+    ++ autostartIf config.programs.thunderbird.enable {command = "thunderbird";}
+    ++ autostartIf (extraArgs.osConfig.networking.networkmanager.enable or false) {command = "nm-applet";}
+    ++ autostartIf
+    (config.personal.profiles.social && config.personal.identities.personal) {
+      command = "signal-desktop";
+    };
 }
