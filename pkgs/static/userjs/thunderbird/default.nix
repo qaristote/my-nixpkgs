f7905797eca00c88b4e87c20e610211c3bf8e65f
aristote <quentin.aristote@irif.fr>
2024-11-08 15:19:22 +0100

    pkgs: thunderbird userjs: update to v128

--- pkgs/static/userjs/thunderbird/default.nix
+++ pkgs/static/userjs/thunderbird/default.nix
@@ -1,7 +1,7 @@
 {fetchurl}: let
-  version = "115.0";
+  version = "128.0";
 in
   fetchurl {
     url = "https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/v${version}/user.js";
-    sha256 = "sha256-66B1yLQkQnydAUXD7KGt32OhWSYcdWX+BUozrgW9uAg=";
+    sha256 = "sha256-V1cTcG52o24bF/0tki/c9+uOdpWaCxqZtIBoSqoQLYk=";
   }
