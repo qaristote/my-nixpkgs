5cb4c8dcf6d880fecf122682e8ad793636e1c3ec
quentin@aristote.fr <quentin@aristote.fr>
2025-01-18 19:53:31 +0100

    pkgs: arkenfox userjs: update

--- pkgs/static/userjs/arkenfox/default.nix
+++ pkgs/static/userjs/arkenfox/default.nix
@@ -1,7 +1,7 @@
 {fetchurl}: let
-  version = "128.0";
+  version = "133.0";
 in
   fetchurl {
     url = "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
-    sha256 = "sha256-CJk9sni0+cYC9rBHSL2mDQRtpsQJobQ1u3tq991Oi1c=";
+    sha256 = "sha256-rPcH24YqEBOzoPB9yxMlke/3tqpi9L7GVMsZ3MUP8WY=";
   }
