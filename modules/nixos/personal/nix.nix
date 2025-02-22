27a7ae1d1933121c250ce7073bde1f83889e6151
quentin@aristote.fr <quentin@aristote.fr>
2025-02-22 15:39:32 +0100

    nixos: nix: enable recursive nix

--- modules/nixos/personal/nix.nix
+++ modules/nixos/personal/nix.nix
@@ -72,7 +72,7 @@ in {
         package = pkgs.lix;
         settings = {
           auto-optimise-store = true;
-          experimental-features = ["nix-command" "flakes"];
+          experimental-features = ["nix-command" "flakes" "recursive-nix"];
           substituters = ["https://devenv.cachix.org/" "https://nix-community.cachix.org/"];
           trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
         };
