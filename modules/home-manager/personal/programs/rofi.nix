1ac03c9467ad9af9f8d8808204300a7bbfdd27d6
aristote <quentin.aristote@irif.fr>
2025-04-28 14:40:20 +0200

    home: rofi: fix config file import

--- modules/home-manager/personal/programs/rofi.nix
+++ modules/home-manager/personal/programs/rofi.nix
@@ -8,12 +8,17 @@
     cycle = lib.mkDefault true;
     theme =
       lib.mkIf (config.lib ? stylix)
-      (lib.mkForce
+      (
+        lib.mkForce
         (config.lib.stylix.colors {
           template = builtins.readFile config.personal.home.dotfiles.rofi;
           extension = ".rasi";
         })
-        .out);
+      );
     plugins = with pkgs; [rofi-top rofi-calc rofi-emoji];
+  };
+  xdg.dataFile."rofi/themes/custom.rasi" = lib.mkForce {
+    source = config.programs.rofi.theme;
+    text = null;
   };
 }
