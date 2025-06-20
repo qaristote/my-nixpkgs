299c18d995458906919a3a03e80bdc791c856615
aristote <quentin.aristote@irif.fr>
2025-06-20 21:25:39 +0200

    nixos: sound: make pipewire default sound server

--- modules/nixos/personal/hardware.nix
+++ modules/nixos/personal/hardware.nix
@@ -63,6 +63,7 @@ in {
       security.rtkit.enable = true;
       services.pipewire = {
         enable = true;
+        audio.enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
