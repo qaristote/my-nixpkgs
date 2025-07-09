904fa66fe7e8f92d2fb9039065b439b0f2f6a622
quentin@aristote.fr <quentin@aristote.fr>
2025-07-09 15:02:39 +0200

    stylix: option renaming

--- modules/home-manager/personal/gui/x/i3/bar/default.nix
+++ modules/home-manager/personal/gui/x/i3/bar/default.nix
@@ -1,18 +1,23 @@
-{ config, lib, pkgs, ... }@extraArgs:
-
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   statusPackage =
-    pkgs.personal.barista.override { i3statusGo = ./i3status.go; };
+    pkgs.personal.barista.override {i3statusGo = ./i3status.go;};
 in {
   xsession.windowManager.i3.config.bars = [
     ({
-      statusCommand = "${statusPackage}/bin/i3status";
-    } // (config.lib.stylix.i3.bar or { colors.background = "#111111"; }) // {
-      fonts = {
-        names = [ "roboto" ];
-        size = 11.0;
-      };
-    })
+        statusCommand = "${statusPackage}/bin/i3status";
+      }
+      // (config.lib.stylix.i3.targets.i3.exportedBarConfig or {colors.background = "#111111";})
+      // {
+        fonts = {
+          names = ["roboto"];
+          size = 11.0;
+        };
+      })
   ];
 
   home.packages = with pkgs;
