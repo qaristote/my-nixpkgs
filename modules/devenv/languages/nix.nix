6f25bf04950cf49363a81449ea9faa52402a00cb
aristote <quentin.aristote@irif.fr>
2025-07-29 14:54:36 +0200

    devenv: nix: default formatter: alejandra -> nixfmt

--- modules/devenv/languages/nix.nix
+++ modules/devenv/languages/nix.nix
@@ -5,28 +5,46 @@
   pkgs,
   devenv,
   ...
-}: let
+}:
+let
   cfg = config.languages.nix;
-in {
-  disabledModules = [(devenv.modules + "/languages/nix.nix")];
+in
+{
+  disabledModules = [ (devenv.modules + "/languages/nix.nix") ];
 
   options.languages.nix = {
     enable = lib.mkEnableOption "tools for nix development";
-    formatter = lib.mkPackageOption pkgs "nix formatter" {
-      default = ["alejandra"];
-    };
+    formatter = lib.mkPackageOption pkgs "nix formatter" { default = [ "nixfmt" ]; };
     packaging.enable = lib.mkEnableOption "tools for writing nix derivations";
   };
 
-  config = lib.mkIf cfg.enable {
-    packages = with pkgs; [cfg.formatter deadnix] ++ lib.optionals cfg.packaging.enable [nix-prefetch-scripts nix-prefetch-github];
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        packages =
+          with pkgs;
+          [
+            cfg.formatter
+            deadnix
+          ]
+          ++ lib.optionals cfg.packaging.enable [
+            nix-prefetch-scripts
+            nix-prefetch-github
+          ];
 
-    git-hooks.hooks = {
-      deadnix.enable = lib.mkDefault true;
-      "${cfg.formatter.pname}".enable = lib.mkDefault true;
-    };
-
-    scripts."${cfg.formatter.pname}-emacs".exec = "${cfg.formatter.pname} " + lib.optionalString (cfg.formatter.pname == "alejandra") "--quiet" + " $@";
-    emacs.dirLocals.nix-mode.nix-nixfmt-bin = ''"${cfg.formatter.pname}-emacs"'';
-  };
+        git-hooks.hooks =
+          let
+            formatterHookname = with cfg.formatter; if pname == "nixfmt" then "nixfmt-rfc-style" else pname;
+          in
+          {
+            deadnix.enable = lib.mkDefault true;
+            "${formatterHookname}".enable = lib.mkDefault true;
+          };
+      }
+      (lib.mkIf (cfg.formatter.pname == "alejandra") {
+        scripts."alejandra-emacs".exec = "alejandra --quiet $@";
+        emacs.dirLocals.nix-mode.nix-nixfmt-bin = ''"alejandra-emacs"'';
+      })
+    ]
+  );
 }
