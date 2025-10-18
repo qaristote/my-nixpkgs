25f53f34cbc0fcb8917a0c0d217288b39f951bbe
quentin@aristote.fr <quentin@aristote.fr>
2025-10-18 11:26:21 +0200

    nixos: add db of ssh public keys

--- modules/nixos/personal/default.nix
+++ modules/nixos/personal/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./boot.nix
@@ -11,4 +11,20 @@
     ./system.nix
     ./user.nix
   ];
+
+  options.personal.lib.publicKeys.ssh = lib.mkOption {
+    type = with lib.types; attrsOf str;
+    default = { };
+    example = {
+      machine = "ssh-ed25519 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA user@domain";
+    };
+    description = "Known SSH public keys.";
+  };
+
+  config.personal.lib.publicKeys.ssh = {
+    latitude-7490 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4wGbl3++lqCjLUhoRyABBrVEeNhIXYO4371srkRoyq qaristote@latitude-7490";
+    precision-3571 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvPsKWQXX/QsFQjJU0CjG4LllvUVZme45d9JeS/yhLt qaristote@precision-3571";
+    dragonfly-g4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICT+jPcQhtBu4jxNAn54PV2TJ5krCfFnbXsR3OHk72l8 qaristote@dragonfly-g4";
+    optiplex-9030 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDh2W0Nv76Nnw8TNysOkxVDZpnW0VEptq4u4Rask6zoO qaristote@optiplex-9030";
+  };
 }
