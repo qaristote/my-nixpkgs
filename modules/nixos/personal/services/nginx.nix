2fd9ddfd4925f85dda9ba2fd7fb5ec5c07864c45
quentin@aristote.fr <quentin@aristote.fr>
2026-01-25 21:18:10 +0100

    nixos: nginx: remove _ virtual host

--- modules/nixos/personal/services/nginx.nix
+++ modules/nixos/personal/services/nginx.nix
@@ -8,14 +8,5 @@
     recommendedProxySettings = lib.mkDefault true;
     recommendedTlsSettings = lib.mkDefault true;
     recommendedUwsgiSettings = lib.mkDefault config.services.uwsgi.enable;
-
-    # return 444 when trying to connect to some unknown virtual host
-    virtualHosts."_" = {
-      default = true;
-      extraConfig = ''
-        return 444;
-      '';
-    };
-
   };
 }
